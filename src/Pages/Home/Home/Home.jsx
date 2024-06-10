d8df665ed52ae5fb488c74c677454595f5d7de28
shammy46 <sanjidashammy81@gmail.com>
2024-06-11 00:44:00 +0600

    testimonials & cover components added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import PopularMenu from "../PopularMenu/PopularMenu";
@@ -8,6 +9,10 @@ import Testimonials from "./Testimonials/Testimonials";
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+                <link rel="canonical" href="https://www.tacobell.com/" />
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,10 +1,24 @@
 //import React from 'react';
 
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg"
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 const Menu = () => {
     return (
         <div>
-            <h2>men men menu</h2>
-            
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+                <link rel="canonical" href="https://www.tacobell.com/" />
+            </Helmet>
+            <Cover
+            img={menuImg}
+            title= "our menu"
+            ></Cover>
+            <PopularMenu></PopularMenu>
+           
+
         </div>
     );
 };
