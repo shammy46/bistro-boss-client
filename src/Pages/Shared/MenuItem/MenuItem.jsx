0d4f3ce35cfe989a65e3b514156d25be2381a878
shammy46 <sanjidashammy81@gmail.com>
2024-06-10 20:18:21 +0600

    our menu section added

---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+
+
+const MenuItem = ({item}) => {
+    const {image, name, price, recipe}= item;
+    return (
+        <div className="flex space-x-4">
+            <img style={{borderRadius: '0 200px 200px'}} className="w-[100px]" src={image} alt="" />
+            <div className="w-[120px">
+                <h3 className="uppercase ">{name}----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">{price}</p>
+            
+        </div>
+    );
+};
+
+export default MenuItem;
